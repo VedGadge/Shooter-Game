8fe4711a36389a262962b3e8f3b383f03c8e526a
VedGadge <96955002+VedGadge@users.noreply.github.com>
2022-06-14 12:24:06 +0530

    Completed Level Editor Using Tilemaps

--- level_editor.py
+++ level_editor.py
@@ -1,4 +1,7 @@
 import pygame
+import button
+import csv
+
 
 pygame.init()
 # set frame rate
@@ -18,17 +21,22 @@ pygame.display.set_caption('Level Editor')
 ROWS = 16
 MAX_COLS = 150
 TILE_SIZE = SCREEN_HEIGHT // ROWS
-
+TILE_TYPES = 21
+level = 0
+current_tile = 0
 scroll_left = False
 scroll_right = False
 scroll = 0
 scroll_speed = 1
+
 
 # define colors
 G = (144, 201, 120)
 W = (255, 255, 255)
 R = (200, 25, 25) 
 
+#define font
+font = pygame.font.SysFont('Futura', 30)
 
 
 #load Images
@@ -36,6 +44,35 @@ pine1_img = pygame.image.load('img/background/pine1.png').convert_alpha()
 pine2_img = pygame.image.load('img/background/pine2.png').convert_alpha()
 mountain_img = pygame.image.load('img/background/mountain.png').convert_alpha()
 sky_img = pygame.image.load('img/background/sky_cloud.png').convert_alpha()
+
+#store tiles in a list
+img_list = []
+for x in range(TILE_TYPES):
+    img = pygame.image.load(f'img/tile/{x}.png').convert_alpha()
+    img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
+    img_list.append(img)
+
+save_img = pygame.image.load('img/save_btn.png').convert_alpha()
+load_img = pygame.image.load('img/load_btn.png').convert_alpha()
+
+
+# create empty tile list
+world_data = []
+for row in range (ROWS):
+    r = [-1] * MAX_COLS
+    world_data.append(r)
+
+#create ground
+for tile in range(0, MAX_COLS):
+    world_data[ROWS - 1][tile] = 0
+
+
+# function for outputting text onto the screen
+
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img,(x, y))
+
 
 # create function for drawing background
 def draw_bg():
@@ -60,6 +97,29 @@ def draw_grid():
         pygame.draw.line(screen, W, (0, c * TILE_SIZE), (SCREEN_WIDTH, c * TILE_SIZE))
 
 
+# function for drawing the world tiles
+def draw_world():
+    for y, row in enumerate(world_data):
+        for x, tile in enumerate(row):
+            if tile >= 0:
+                screen.blit(img_list[tile], (x * TILE_SIZE - scroll, y * TILE_SIZE))
+
+
+# create buttom
+save_button = button.Button(SCREEN_WIDTH // 2, SCREEN_HEIGHT + LOWER_MARGIN - 50, save_img, 1)
+load_button = button.Button(SCREEN_WIDTH // 2 + 200, SCREEN_HEIGHT + LOWER_MARGIN - 50, load_img, 1)
+# make a button list
+button_list = []
+button_col = 0
+button_row = 0
+for i in range(len(img_list)):
+    tile_button = button.Button(SCREEN_WIDTH + (75 * button_col) + 50, (75 *button_row) + 50, img_list[i], 1)
+    button_list.append(tile_button)
+    button_col += 1
+    if button_col == 3:
+        button_row += 1
+        button_col = 0
+
 
 run = True
 while run:
@@ -68,14 +128,67 @@ while run:
 
     draw_bg()
     draw_grid()
+    draw_world()
+    draw_text(f'Level: {level}', font, W, 10, SCREEN_HEIGHT + LOWER_MARGIN - 90)
+    draw_text('Press UP or DOWN to change level', font, W, 10, SCREEN_HEIGHT + LOWER_MARGIN - 60)
+
+    # save and load data --> Used csv module: generates csv file, can try with pickle module also but it will return python list
+    
+    if save_button.draw(screen):
+        # save level data
+        with open(f'level{level}_data.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter= ',')
+            for row in world_data:
+                writer.writerow(row)
+    if load_button.draw(screen):
+        # load in level data 
+        # reset scroll back to the start level
+        scroll = 0
+        with open(f'level{level}_data.csv', newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter= ',')
+            for x, row in enumerate(reader):
+                for y, tile in enumerate(row):
+                    world_data[x][y] = int(tile)
+                
+
+
+
+    #draw tile panel and tiles
+    pygame.draw.rect(screen, G, (SCREEN_WIDTH, 0, SIDE_MARGIN, SCREEN_HEIGHT))
+
+    # choose a tile
+    button_count = 0
+    for button_count, i in enumerate(button_list):
+       if i.draw(screen):
+        current_tile = button_count
+
+    # highlight the selected tile
+    pygame.draw.rect(screen, R, button_list[current_tile].rect, 3)
 
 
 
     # scroll the  map
     if scroll_left == True and scroll > 0:
         scroll -= 5 * scroll_speed
-    if scroll_right == True:
+    if scroll_right == True and scroll < (MAX_COLS * TILE_SIZE) - SCREEN_WIDTH:
         scroll += 5 * scroll_speed
+
+
+    # add new tiles to the screen
+    # get mouse position
+    pos = pygame.mouse.get_pos()
+    x = (pos[0] + scroll) // TILE_SIZE
+    y = pos[1] // TILE_SIZE
+
+    # check that the coordinates are within the tile area
+    if pos[0] < SCREEN_WIDTH and pos[1] < SCREEN_HEIGHT:
+        # update tile value
+        if pygame.mouse.get_pressed()[0] == 1: # 0 --> left click
+            if world_data[y][x] != current_tile:
+                 world_data[y][x] = current_tile
+        if pygame.mouse.get_pressed()[2] == 1: # 2 --> right click
+            world_data[y][x] = -1
+
 
 
     for event in pygame.event.get(): # Gets all the events that are happening
@@ -84,6 +197,10 @@ while run:
             run = False
           # keyboard presses
         if event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_UP:
+                level += 1
+            if event.key == pygame.K_DOWN and level > 0:
+                level -= 1
             if event.key == pygame.K_LEFT:
                 scroll_left = True
             if event.key == pygame.K_RIGHT:
